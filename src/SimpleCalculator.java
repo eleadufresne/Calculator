5576a30b7ab5575d6d193c2c1793a53ec3f652a2
Éléa Dufresne <elea.dufresne@mail.mcgill.ca>
2023-03-10 17:18:19 -0500

    first attempt

--- src/SimpleCalculator.java
+++ src/SimpleCalculator.java
@@ -13,249 +13,231 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
 
 //This is a simple calculator
-//Coder : Elea Dufresne 
+//Coder : Elea Dufresne
 //Date: 2020-07-17
 
-   public class SimpleCalculator extends JFrame implements ActionListener{
+public class SimpleCalculator extends JFrame implements ActionListener {
     // Panel (container)
-        JPanel container = new JPanel();
+    JPanel container = new JPanel();
     // Display screen
-        JLabel screen = new JLabel();
+    JLabel screen = new JLabel();
     //Commands and result
-        String num1, op, num2;
+    String num1, op, num2;
     //Checks if there is already a dot
-        private boolean isDot = false;
+    private boolean hasDot = false;
 
-    // Buttons
-     Button btn0 = new Button("0"); 
-     Button btn1 = new Button("1"); 
-     Button btn2 = new Button("2"); 
-     Button btn3 = new Button("3"); 
-     Button btn4 = new Button("4"); 
-     Button btn5 = new Button("5"); 
-     Button btn6 = new Button("6"); 
-     Button btn7 = new Button("7"); 
-     Button btn8 = new Button("8"); 
-     Button btn9 = new Button("9"); 
+    // buttons
+    Button[] numbers = new Button[10];
+    Button dot = new Button(".");
+    Button equality = new Button("=");
+    Button C = new Button("C");
+    Button add = new Button("+");
+    Button subtract = new Button("-");
+    Button multiply = new Button("x");
+    Button divide = new Button("÷");
 
-     Button dot= new Button(".");
-     Button equality = new Button("=");
-
-     Button C = new Button("C");
-     Button add = new Button("+");
-     Button subtract = new Button("-");
-     Button multiply = new Button("x");
-     Button divide = new Button("÷");
-
-
-   public SimpleCalculator() {
-	
-	//Setting the commands to nothing by default
-    
-          num1=op=num2="";
-
-    // Window properties 
-       this.setTitle("Calculator"); 
-          this.setSize(600,700);
-             this.setLocationRelativeTo(null);
-                this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-    // Panel
-        container.setBackground(new Color(51,102,0));
-      container.setLayout(new BorderLayout());
-      
-/*** Layout and disposition of buttons and screen *********************************************************************************/
-
-      // screen 
-        screen.setPreferredSize(new Dimension(2,175)); //makes it visible
-          screen.setFont( new Font ("Bahnschrift", Font.PLAIN, 50) );
-            screen.setForeground(new Color(16, 37, 3));
-              screen.setHorizontalAlignment(JLabel.RIGHT);
-                screen.setBorder(new LineBorder(Color.black, 17)); //Note that EmptyBorder does not work
-                 container.add(screen, BorderLayout.NORTH); //screen at the top
-
-     
-     // numbers, . and = 
-       JPanel num = new JPanel();
-     
-         num.setBackground(Color.black);
-     
-            GridLayout grid1 = new GridLayout(4,3);
-     
-        grid1.setVgap(5);
-        grid1.setHgap(5);
-     
-       num.setLayout(grid1);   
-     
-     num.add(btn7);
-     num.add(btn8);
-     num.add(btn9);
-     num.add(btn4);
-     num.add(btn5);
-     num.add(btn6);
-     num.add(btn1);
-     num.add(btn2);
-     num.add(btn3);
-     num.add(btn0);
-     num.add(dot);
-     num.add(equality);
-     
-       num.setBorder(new EmptyBorder(40, 40, 40, 40));
-
-         container.add(num, BorderLayout.CENTER); //numbers in the middle-left
- 
-     // operators and C
-     JPanel operators = new JPanel();
-     
-      operators.setBackground(Color.black);
-     
-       GridLayout grid2 = new GridLayout(5,1);
-     
-        grid2.setVgap(5);
-        grid2.setHgap(5);
-     
-     operators.setLayout(grid2);
-   
-       operators.add(C);
-       operators.add(add);
-       operators.add(subtract);
-       operators.add(multiply);
-       operators.add(divide);
-       
-     operators.setBorder(new EmptyBorder( 20, 0, 20, 40));
-       
-       operators.setPreferredSize(new Dimension(125,600));
-       
-     container.add(operators, BorderLayout.EAST); // operators on the right   
-   
-     //  So the button listener class can listen to the buttons
-             btn0.addActionListener(this);
-             btn1.addActionListener(this);
-             btn2.addActionListener(this);
-             btn3.addActionListener(this); 
-             btn4.addActionListener(this); 
-             btn5.addActionListener(this); 
-             btn6.addActionListener(this); 
-             btn7.addActionListener(this); 
-             btn8.addActionListener(this); 
-             btn9.addActionListener(this); 
-             dot.addActionListener(this); 
-             equality.addActionListener(this); 
-             C.addActionListener(this); 
-             add.addActionListener(this); 
-             subtract.addActionListener(this); 
-             multiply.addActionListener(this); 
-             divide.addActionListener(this); 
-     
-         
-         this.setContentPane(container);
-         this.setVisible(true);	
-
+    public static void main(String[] args) {
+        SimpleCalculator calculator = new SimpleCalculator();
     }
 
-   //action listener interface's method
-   public void actionPerformed(ActionEvent arg0) {
-	//String corresponding to pushed button
-        String command = arg0.getActionCommand();
-    //if it's a number or a dot
-        if ( command.charAt(0)>='0' && command.charAt(0)<='9' || command.charAt(0) == '.' ) {
-    	    //if an operator has been pressed
-    	    if(!op.equals("")) {
-                if(command.charAt(0) == '.' && isDot){
-                //prevents having two dots in a given number
-                } else if(command.charAt(0) == '.' && !isDot){
-                    num2 = num2 + command;
-                    screen.setText(num2 + "  ");
-                    isDot=true;
-                }else{
-                    num2 = num2 + command;
-                    screen.setText(num2 + "  ");
-                }
-        //if no operator was pressed prior to this number
-        }else
-            if(command.charAt(0) == '.' && isDot){
+    public SimpleCalculator() {
+        //Setting the commands to nothing by default
+        num1 = op = num2 = "";
 
-            }else if(command.charAt(0) == '.' && !isDot){
-                num1 = num1 + command;
-                screen.setText(num1+"  ");
-                isDot=true;
-            }else {
-                num1 = num1 + command;
+        // Window properties
+        this.setTitle("Calculator");
+        this.setSize(600, 700);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Panel
+        container.setBackground(new Color(51, 102, 0));
+        container.setLayout(new BorderLayout());
+
+        /*** Layout and disposition of buttons and screen **********************************************************/
+
+        // screen
+        screen.setPreferredSize(new Dimension(2, 175)); //makes it visible
+        screen.setFont(new Font("Bahnschrift", Font.PLAIN, 50));
+        screen.setForeground(new Color(16, 37, 3));
+        screen.setHorizontalAlignment(JLabel.RIGHT);
+        screen.setBorder(new LineBorder(Color.black, 17)); //Note that EmptyBorder does not work
+        container.add(screen, BorderLayout.NORTH); //screen at the top
+
+        // layout and panel for the numpad, "." and "="
+        GridLayout grid1 = new GridLayout(4, 3);
+        grid1.setVgap(5);
+        grid1.setHgap(5);
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setBackground(Color.black);
+        buttonPanel.setLayout(grid1);
+
+        // instantiate the numpad
+        for(int i=0; i<10; i++) {
+            numbers[i] = new Button(Integer.toString(i));
+            numbers[i].addActionListener(this);
+            buttonPanel.add(numbers[i]);
+        }
+        dot.addActionListener(this);
+        buttonPanel.add(dot);
+        equality.addActionListener(this);
+        buttonPanel.add(equality);
+
+        // format it and then add it to the container
+        buttonPanel.setBorder(new EmptyBorder(40, 40, 40, 40));
+        container.add(buttonPanel, BorderLayout.CENTER);
+
+        // layout and panel for the operators and C
+        GridLayout grid2 = new GridLayout(5, 1);
+        grid2.setVgap(5);
+        grid2.setHgap(5);
+        JPanel operators = new JPanel();
+        operators.setBackground(Color.black);
+        operators.setLayout(grid2);
+        // add the operators
+        C.addActionListener(this);
+        add.addActionListener(this);
+        subtract.addActionListener(this);
+        multiply.addActionListener(this);
+        divide.addActionListener(this);
+        operators.add(C);
+        operators.add(add);
+        operators.add(subtract);
+        operators.add(multiply);
+        operators.add(divide);
+        // format and then add the panel to the container
+        operators.setBorder(new EmptyBorder(20, 0, 20, 40));
+        operators.setPreferredSize(new Dimension(125, 600));
+        container.add(operators, BorderLayout.EAST);
+
+        // put everything together
+        this.setContentPane(container);
+        this.setVisible(true);
+    }
+
+    // helper method for setting the screen display
+    private void setDisplay(String command, String num){
+        if (command.charAt(0) == '.' && hasDot) {
+        // if we are not introducing a second dot
+        } else if (command.charAt(0) == '.' && !hasDot) {
+            num = num + command;
+            screen.setText(num + "  ");
+            hasDot = true;
+        } else {
+            num = num + command;
+            screen.setText(num + "  ");
+        }
+    }
+
+    private void compute(String cmd){
+        if (op.equals("+")) {
+            num1 = String.valueOf(Double.parseDouble(num1) + Double.parseDouble(num2));
+            op = cmd;
+            num2 = "";
+            screen.setText(num1 + "  ");
+        } else if (op.equals("-")) {
+            num1 = String.valueOf(Double.parseDouble(num1) - Double.parseDouble(num2));
+            op = cmd;
+            num2 = "";
+            screen.setText(num1 + "  ");
+        } else if (op.equals("x")) {
+            num1 = String.valueOf(Double.parseDouble(num1) * Double.parseDouble(num2));
+            op = cmd;
+            num2 = "";
+            screen.setText(num1 + "  ");
+        } else if (op.equals("÷")) {
+            // edge case : we are trying to divide by zero
+            if( num2.equals("0")) {
+                num1 = op = num2 = "";
+                screen.setText("ERROR : NaN");
+            } else {
+                num1 = String.valueOf(Double.parseDouble(num1) / Double.parseDouble(num2));
+                op = cmd;
+                num2 = "";
                 screen.setText(num1 + "  ");
             }
-        }//if C is pressed
-        else if ( command.charAt(0)=='C' ) {
-    	//everything is reset
-    	    num1=op=num2= "";
-    	    screen.setText("");
-            isDot=false;
-        }//if equal is pressed
-        else if ( command.charAt(0)=='=' )  {
-            isDot=false;
-            //if a given operator was pressed before the equal
-            if ( op.equals("+") )  {
-    	                                      num1=String.valueOf(Double.valueOf(num1)+Double.valueOf(num2));
-    	                                      op=num2="";
-    	                                      screen.setText(num1+"  ");
-            } else if ( op.equals("-") ) {
-    	                                      num1=String.valueOf(Double.valueOf(num1)-Double.valueOf(num2));
-    	                                      op=num2="";
-    	                                      screen.setText(num1+"  ");
-    	                                      
-            } else if ( op.equals("x") ) {
-    	                                      num1=String.valueOf(Double.valueOf(num1)*Double.valueOf(num2));
-    	                                      op=num2="";
-    	                                      screen.setText(num1+"  ");
-    
-            } else if ( op.equals("÷") && !(num2.equals("0")) )  {
-    	                                      num1=String.valueOf(Double.valueOf(num1)/Double.valueOf(num2));
-    	                                      op=num2="";
-    	                                      screen.setText(num1+"  ");
-            } else if (op.equals("÷") && num2.equals("0"))  {
-    	                                     num1=op=num2="";
-    	                                     screen.setText("ERROR  "); 
+        }
+    }
+
+    //action listener interface's method
+    public void actionPerformed(ActionEvent arg0) {
+        // button the user pressed
+        String command = arg0.getActionCommand();
+        // if the command is a digit or a dot
+        if (command.charAt(0) >= '0' && command.charAt(0) <= '9' || command.charAt(0) == '.') {
+            // if no operator was pressed before to this digit
+            if (op.equals("")) setDisplay(command, num1);
+                // if an operator had been pressed
+            else setDisplay(command, num2);
+        // if C was pressed
+        } else if (command.charAt(0) == 'C') {
+            // reset everything
+            num1 = op = num2 = "";
+            screen.setText("");
+            hasDot = false;
+        } // if the equal operator is pressed
+        else if (command.charAt(0) == '=') {
+            // perform the computation
+            hasDot = false;
+            if (op.equals("+")) {
+                num1 = String.valueOf(Double.parseDouble(num1) + Double.parseDouble(num2));
+                op = num2 = "";
+                screen.setText(num1 + "  ");
+            } else if (op.equals("-")) {
+                num1 = String.valueOf(Double.parseDouble(num1) - Double.parseDouble(num2));
+                op = num2 = "";
+                screen.setText(num1 + "  ");
+            } else if (op.equals("x")) {
+                num1 = String.valueOf(Double.parseDouble(num1) * Double.parseDouble(num2));
+                op = num2 = "";
+                screen.setText(num1 + "  ");
+            } else if (op.equals("÷")) {
+                // edge case : we are trying to divide by zero
+                if( num2.equals("0")) {
+                    num1 = op = num2 = "";
+                    screen.setText("ERROR : NaN");
+                } else {
+                    num1 = String.valueOf(Double.parseDouble(num1) / Double.parseDouble(num2));
+                    op = num2 = "";
+                    screen.setText(num1 + "  ");
+                }
             }
+        // if the command is an operator
         } else {
-            isDot=false;
-            //if no second number was pressed
-    	    if( op.equals("") || num2.equals(""))	op=command; //assures that op is an operator
-            //else evaluate
+            hasDot = false;
+            if (op.equals("") || num2.equals(""))
+                op = command;
             else {
-                //if a given operator was pressed
-    	        if ( op.equals("+") )  {
-                    num1=String.valueOf(Double.valueOf(num1)+Double.valueOf(num2));
+                if (op.equals("+")) {
+                    num1 = String.valueOf(Double.parseDouble(num1) + Double.parseDouble(num2));
                     op = command;
                     num2 = "";
-                    screen.setText(num1+"  ");
+                    screen.setText(num1 + "  ");
 
-                } else if ( op.equals("-") ) {
-                    num1=String.valueOf(Double.valueOf(num1)-Double.valueOf(num2));
+                } else if (op.equals("-")) {
+                    num1 = String.valueOf(Double.parseDouble(num1) - Double.parseDouble(num2));
                     op = command;
                     num2 = "";
-                    screen.setText(num1+"  ");
-                
-                } else if ( op.equals("x") ) {
-                    num1=String.valueOf(Double.valueOf(num1)*Double.valueOf(num2));
-                    op = command;
-                    num2 = "";
-                    screen.setText(num1+"  ");
+                    screen.setText(num1 + "  ");
 
-                }else if ( op.equals("÷") && !(num2.equals("0")) )  {
-                    num1=String.valueOf(Double.valueOf(num1)/Double.valueOf(num2));
+                } else if (op.equals("x")) {
+                    num1 = String.valueOf(Double.parseDouble(num1) * Double.parseDouble(num2));
                     op = command;
                     num2 = "";
-                    screen.setText(num1+"  ");
-                }else if (op.equals("÷") && num2.equals("0"))  {
-                    num1=op=num2="";
+                    screen.setText(num1 + "  ");
+
+                } else if (op.equals("÷") && !(num2.equals("0"))) {
+                    num1 = String.valueOf(Double.parseDouble(num1) / Double.parseDouble(num2));
+                    op = command;
+                    num2 = "";
+                    screen.setText(num1 + "  ");
+                } else if (op.equals("÷") && num2.equals("0")) {
+                    num1 = op = num2 = "";
                     screen.setText("ERROR  ");
                 }
             }//continues to do operations even if equals was not pressed
-            screen.setText(num1+"  ");
+            screen.setText(num1 + "  ");
         }
-    	
     }
-	public static void main(String[] args) {
-		SimpleCalculator calculator = new SimpleCalculator();
-	}
 }
 	
